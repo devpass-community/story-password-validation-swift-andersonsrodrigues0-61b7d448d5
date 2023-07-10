0200d6ee95b1dc577f0d4c87fb5e4fbb087313b8
Anderson Rodrigues <srodrigues.anderson@gmail.com>
2023-07-10 15:44:26 -0300

    Implemented regex

--- Sources/PasswordValidation/PasswordValidation.swift
+++ Sources/PasswordValidation/PasswordValidation.swift
@@ -17,7 +17,12 @@ public struct PasswordValidation {
     }
 
     func isValid(password: String) -> Bool {
-
-        return false
+        do {
+            let range = NSRange(location: 0, length: password.utf16.count)
+            let regex = try NSRegularExpression(pattern: "^[0-9]{6}$")
+            return regex.matches(in: password, range: range).count > 0
+        } catch {
+            return false
+        }
     }
 }
